f25da8c55f8ab6bb5aa7c80e19087c6708bbbfac
Matthew Ragan <raganmd@gmail.com>
2023-07-11 12:34:00 -0700

    Resolves base URL config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://td-style.guide',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/TouchDesigner-StyleGuide',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
